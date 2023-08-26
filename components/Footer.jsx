53836c58ca9c537f722aa08434e1a788626d6b8c
shrey-dev-2023 <workwithshreyarts@gmail.com>
2023-08-26 14:37:29 +0530

    Error-2

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,10 @@
 "use client";
+
 import { motion } from "framer-motion";
 import { socials } from "../constants";
 import styles from "../styles";
 import { footerVariants } from "../utils/motion";
+
 const Footer = () => (
   <motion.footer
     variants={footerVariants}
